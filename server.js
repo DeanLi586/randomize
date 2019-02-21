59deecc66416dc18b7ffd0868a9842557e0d89f3
Dean Li <lazerx506@gmail.com>
2019-02-21 16:13:51 +0000

    added route to seed database with frameworks data

--- server.js
+++ server.js
@@ -3,15 +3,18 @@ const express = require("express");
 const app = express();
 const db = require("./db/db");
 const logger = require("./config/logger");
+const testRoutes = require('./routes/test.routes');
 // const swagger = require("swagger-ui-express");
 // const swaggerDocs = require("./swagger.json");
 
 const PORT = process.env.PORT || 3000;
 
-
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({
+    extended: false
+}));
 // app.use('/api-docs', swagger.serve, swagger.setup(swaggerDocs));
 
+app.use(testRoutes);
 
 app.listen(PORT, () => {
     logger.info(`Server running on PORT=${PORT}`);
