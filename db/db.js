92e2ca6e5eaa07b3154eca38dfa794ef123e53c6
Dean Li <lazerx506@gmail.com>
2019-02-22 23:33:47 +0000

    moved seed method into db connection and removed seed route

--- db/db.js
+++ db/db.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const logger = require('../config/logger');
+const Frameworks = require('../utils/seed');
+const framework = require('../models/framework');
 
 const DB_USER = process.env.DB_USER || "Nexus586";
 const DB_NAME = process.env.DB_NAME || "randomize";
@@ -12,10 +14,29 @@ const DB_URL = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`;
 
 mongoose.connect(DB_URL, {useNewUrlParser: true})
 .then(() => {
-    logger.info("Successfully connected to db");
+    mongoose.connection.db.collection("frameworks").countDocuments((err, count) => {
+        if(err){
+            logger.error(`An error occurred: ${err}`);
+        }
+
+        if(count === 0){
+            Frameworks.forEach((elememt) => {
+                let newFramework = new framework(elememt);
+                newFramework.save()
+                .then(() => {
+                    logger.info(`framework with name: ${newFramework.name} has been saved`);
+                })
+                .catch((err) => {
+                    logger.error(err);
+                });
+            });
+        }else {
+            logger.info(`number of items in the collection: ${count}`);
+        }
+    });
 })
 .catch((err) => {
     logger.error(err);
 });
-
+// the time jesus comot the demons make them enter the pigs inside wey them go fall the see inside 
 module.exports = mongoose;
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require("express");
 const app = express();
 const db = require("./db/db");
 const logger = require("./config/logger");
-const testRoutes = require('./routes/test_routes');
 // const swagger = require("swagger-ui-express");
 // const swaggerDocs = require("./swagger.json");
 
@@ -14,7 +13,6 @@ app.use(express.urlencoded({
 }));
 // app.use('/api-docs', swagger.serve, swagger.setup(swaggerDocs));
 
-app.use(testRoutes);
 
 app.listen(PORT, () => {
     logger.info(`Server running on PORT=${PORT}`);
