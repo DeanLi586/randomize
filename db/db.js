964098ace638b731d0e64ce765d392bb842e72de
Dean Li <lazerx506@gmail.com>
2019-02-28 17:52:11 +0000

    added method to seed databases and routes to add and delete frameworks and databases

--- db/db.js
+++ db/db.js
@@ -1,42 +1,70 @@
 const mongoose = require("mongoose");
 const logger = require('../config/logger');
-const Frameworks = require('../utils/seed');
+const Frameworks = require('../utils/seed').Frameworks;
+const Databases = require('../utils/seed').Databases;
+const database = require('../models/databases');
 const framework = require('../models/framework');
+
+
 
 const DB_USER = process.env.DB_USER || "Nexus586";
 const DB_NAME = process.env.DB_NAME || "randomize";
-const DB_PASSWORD = process.env.DB_PASSWORD || "NeznW5xV9iYLdud" ;
+const DB_PASSWORD = process.env.DB_PASSWORD || "NeznW5xV9iYLdud";
 const DB_HOST = process.env.DB_HOST || "ds127115.mlab.com:27115";
 
 mongoose.Promise = global.Promise;
 
 const DB_URL = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`;
 
-mongoose.connect(DB_URL, {useNewUrlParser: true})
-.then(() => {
-    mongoose.connection.db.collection("frameworks").countDocuments((err, count) => {
-        if(err){
-            logger.error(`An error occurred: ${err}`);
-        }
+mongoose.connect(DB_URL, {
+        useNewUrlParser: true,
+        useCreateIndex: true
+    })
+    .then(() => {
+        mongoose.connection.db.collection("frameworks").countDocuments((err, count) => {
+            if (err) {
+                logger.error(`An error occurred: ${err}`);
+            }
 
-        if(count === 0){
-            Frameworks.forEach((elememt) => {
-                let newFramework = new framework(elememt);
-                newFramework.save()
-                .then(() => {
-                    logger.info(`framework with name: ${newFramework.name} has been saved`);
-                })
-                .catch((err) => {
-                    logger.error(err);
+            if (count === 0) {
+                Frameworks.forEach((elememt) => {
+                    let newFramework = new framework(elememt);
+                    newFramework.save()
+                        .then(() => {
+                            logger.info(`framework with name: ${newFramework.name} has been saved`);
+                        })
+                        .catch((err) => {
+                            logger.error(err);
+                        });
                 });
-            });
-        }else {
-            logger.info(`number of items in the collection: ${count}`);
-        }
+            } else {
+                logger.info(`number of items in the collection: ${count}`);
+            }
+        });
+
+        mongoose.connection.db.collection('databases').countDocuments((err, count) => {
+            if (err) {
+                logger.error(`An error occurred: ${err}`);
+            }
+
+            if (count == 0) {
+                Databases.forEach((element) => {
+                    let newDatabase = new database(element);
+                    newDatabase.save()
+                        .then(() => {
+                            logger.info(`database with name: ${newDatabase.name} has been saved`);
+                        })
+                        .catch((err) => {
+                            logger.error(err);
+                        });
+                });
+            } else {
+                logger.info(`number of items in the collection: ${count}`);
+            }
+        });
+    })
+    .catch((err) => {
+        logger.error(err);
     });
-})
-.catch((err) => {
-    logger.error(err);
-});
-// the time jesus comot the demons make them enter the pigs inside wey them go fall the see inside 
+    
 module.exports = mongoose;
--- server.js
+++ server.js
@@ -3,16 +3,18 @@ const express = require("express");
 const app = express();
 const db = require("./db/db");
 const logger = require("./config/logger");
+const frameworkRoutes = require('./routes/frameworkRoutes');
 // const swagger = require("swagger-ui-express");
 // const swaggerDocs = require("./swagger.json");
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({
-    extended: false
-}));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 // app.use('/api-docs', swagger.serve, swagger.setup(swaggerDocs));
 
+
+app.use(frameworkRoutes);
 
 app.listen(PORT, () => {
     logger.info(`Server running on PORT=${PORT}`);
